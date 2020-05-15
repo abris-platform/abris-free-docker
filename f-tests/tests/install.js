d936b1c6fc4a044f52babc358d20b3edde6a2c39
Pavel Platonov <devel_27@mail.ru>
2020-05-15 15:31:37 +0300

    Abris install test

--- f-tests/tests/install.js
+++ f-tests/tests/install.js
@@ -18,18 +18,20 @@ test('Abris install', async t => {
         // .pressKey('ctrl+a')
         // .typeText(Selector('div.abris-property.readwrite').find('input[data-bind="value: $data.serverAddress"]'),
         //     'localhost')
-        .click(Selector('div.abris-property.readwrite').find('input[data-bind="value: $data.serverPort"]'))
-        .pressKey('ctrl+a')
-        .typeText(Selector('div.abris-property.readwrite').find('input[data-bind="value: $data.serverPort"]'),
-            '45432')
-        .click(Selector('div.abris-property.readwrite').find('input[data-bind="value: $data.serverPassword"]'))
-        .pressKey('ctrl+a')
-        .typeText(Selector('div.abris-property.readwrite').find('input[data-bind="value: $data.serverPassword"]'),
-            '123456')
+        // .click(Selector('div.abris-property.readwrite').find('input[data-bind="value: $data.serverPort"]'))
+        // .pressKey('ctrl+a')
+        // .typeText(Selector('div.abris-property.readwrite').find('input[data-bind="value: $data.serverPort"]'),
+        //     '45432')
+        // .click(Selector('div.abris-property.readwrite').find('input[data-bind="value: $data.serverPassword"]'))
+        // .pressKey('ctrl+a')
+        // .typeText(Selector('div.abris-property.readwrite').find('input[data-bind="value: $data.serverPassword"]'),
+        //     '123456')
+        .click(Selector('abris-bool.abris-property-createDatabase').find('input'))
         // checkbox'ы
         .takeScreenshot("/" + nameTest + "_1.png")
         .click(Selector('button.btn.btn-default.btn-green.abris-action-right').withText('Install'))
-        .expect(Selector('div.alert.in.fade.alert-success'))
+        .wait(60000)
+        .expect(Selector('iframe #document body').withText('Install completed'))
             .ok('Install failed.')
         .takeScreenshot("/" + nameTest + "_2.png");
 	}

8c122cb9486a2d6fbd7fb485f734652cf4d64f24
Pavel Platonov <devel_27@mail.ru>
2020-05-18 15:40:29 +0300

    Restruct tests

--- f-tests/tests/abris-free.js
+++ f-tests/tests/abris-free.js
@@ -3,10 +3,29 @@ import URL, { Page } from '../modules/page-model';
 
 const url = new URL();
 
-fixture `Authorization`
+fixture `Abris-free`
     .page(url.home);
 
 const page = new Page();
+
+test('Abris install', async t => {
+	var nameTest = "abris_install";
+	await t
+        .resizeWindow(1366, 768)
+        .navigateTo(url.install)
+        // .click(Selector('div.abris-property.readwrite').find('input[data-bind="value: $data.serverPassword"]'))
+        // .pressKey('ctrl+a')
+        // .typeText(Selector('div.abris-property.readwrite').find('input[data-bind="value: $data.serverPassword"]'),
+        //     '123456')
+        .click(Selector('abris-bool.abris-property-createDatabase').find('input'))
+        .takeScreenshot("/" + nameTest + "_1.png")
+        .click(Selector('button.btn.btn-default.btn-green.abris-action-right').withText('Install'))
+        .wait(60000)
+        .expect(Selector('iframe #document body').withText('Install completed'))
+            .ok('Install failed.') // Возможно не совсем верно работает.
+        .takeScreenshot("/" + nameTest + "_2.png");
+	}
+);
 
 test('Login', async t => {
 	var nameTest = "login";

45e28b7bccc0fe889b3a6bbc6e993e6f30aa14d8
Pavel Platonov <devel_27@mail.ru>
2020-05-18 18:10:48 +0300

    Draft project create test. abris#64

--- f-tests/tests/abris-free.js
+++ f-tests/tests/abris-free.js
@@ -24,8 +24,7 @@ test('Abris install', async t => {
         .expect(Selector('iframe #document body').withText('Install completed'))
             .ok('Install failed.') // Возможно не совсем верно работает.
         .takeScreenshot("/" + nameTest + "_2.png");
-	}
-);
+});
 
 test('Login', async t => {
 	var nameTest = "login";
@@ -41,8 +40,7 @@ test('Login', async t => {
 		.expect(page.loginMenu.innerText)
 			.eql('postgres');
 		// .takeScreenshot("/" + nameTest + "_2.png");
-	}
-);
+});
 
 test('Logout', async t => {
 	var nameTest = "logout";
@@ -61,5 +59,44 @@ test('Logout', async t => {
 		.expect(page.loginMenu.innerText)
 			.eql('Вход')		
 		// .takeScreenshot("/" + nameTest + "_2.png");
-	}
-);
+});
+
+test('Create project table', async t => {
+    var nameTest = "project_table";
+    t.expect(page.login());
+    await t
+        .resizeWindow(1366, 768)
+        .navigateTo(url.home)
+        .click(Selector('.abrs-home-item-name-meta'))
+        .click(Selector('.abrs-home-item-name-meta.schema'))
+        .click(Selector('div.table-responsive tbody').find('tr#public'));
+    if (await Selector('div.panel-heading.clearfix.collapsed').withText('Entities').exists) {
+        await t.click(Selector('.panel-heading-caption.left').withText('Entities'))
+    }
+    await t
+        .click(Selector('div.abris-detail-schema div.panel.panel-default').find('.btn-default.btn-blue'))
+        .typeText(Selector('div.abris-detail-entity .abris-property-table_name').find('input'), 
+            'project')
+        .typeText(Selector('div.abris-detail-entity .abris-property-title').find('input'),
+            'Project')
+        .click(Selector('div.abris-detail-entity').find('.btn-default.btn-green'))
+        .click(Selector('div.panel.panel-default div.table-responsive tbody').find('tr').nth(0));
+    if (await Selector('div.panel-heading.clearfix.collapsed').withText('Properties').exists) {
+        await t.click(Selector('.panel-heading-caption.left').withText('Properties'))
+    }
+    await t
+        .click(Selector('.panel-heading-caption.left').withText('Properties').find('.btn-default.btn-blue'))
+        .typeText(Selector('div.abris-detail-property .abris-property-column_name').find('input'),
+            'name')
+        .typeText(Selector('div.abris-detail-property .abris-property-title').find('input'),
+            'Name')
+        .click(Selector('div.abris-detail-property .abris-property-data_type').find('.select2-selection.select2-selection--single'))
+        .typeText(Selector('.select2-search__field'), 
+            'text')
+        .click(Selector('.select2-results__option').withText('text'))
+        .click(Selector('div.abris-detail-property .abris-property-type').find('.select2-selection.select2-selection--single'))
+        .typeText(Selector('.select2-search__field'), 
+            'caption')
+        .click(Selector('.select2-results__option').withText('caption Headline'))        
+        .click(Selector('div.abris-detail-property').find('.btn-default.btn-green'));
+});

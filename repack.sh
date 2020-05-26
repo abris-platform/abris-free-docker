6613eb7d9c07e01520243c36a24e3955b09cdc3e
Pavel Platonov <devel_27@mail.ru>
2020-05-26 15:44:01 +0300

    Some minor fixes

--- repack.sh
+++ repack.sh
@@ -4,6 +4,6 @@ cd /tmp/abris-free && rm -Rf ./*
 curl --output abris-ui.zip --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://abris.site:8091/api/v4/projects/5/jobs/artifacts/master/download?job=build-free";
 unzip ./abris-ui.zip
 curl --output abris-server-base.zip --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://abris.site:8091/api/v4/projects/69/jobs/artifacts/master/download?job=build";
-cd ./dist/ && unzip ./abris-server-base.zip
+cd ./dist/ && unzip ../abris-server-base.zip
 cd ../ && zip -r abris-free.zip ./dist/*
 mv abris-free.zip /var/www/comsite/data/www/abrisplatform.com/downloads/

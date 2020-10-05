415d1082604ae5819d0256d2c1c356b992fd86dd
Aleksey Novikov <novikov82@gmail.com>
2020-10-05 10:45:28 +0300

    Update repack.sh

--- repack.sh
+++ repack.sh
@@ -4,10 +4,6 @@ mkdir -p /tmp/abris-free/dist/images/ && cd /tmp/abris-free
 curl --output abris-ui.zip --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://abris.site:8091/api/v4/projects/5/jobs/artifacts/master/download?job=build-free";
 unzip ./abris-ui.zip
 cp -r ./packages/abris-ui/dist/* ./dist/
-curl --output abris-css.zip --location --header "PRIVATE-TOKEN: z848HojAT-FXxsMmcFnJ" "http://abris.site:8090/api/v4/projects/57/repository/archive.zip"
-unzip ./abris-css.zip
-cp css-master-*/custom.css ./dist/
-cp css-master-*/images/*.png ./dist/images/
 curl --output abris-server-base.zip --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://abris.site:8091/api/v4/projects/69/jobs/artifacts/master/download?job=build";
 cd ./dist/ && unzip ../abris-server-base.zip
 zip -r abris-free.zip ./*

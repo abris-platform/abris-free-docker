5eff75dc5b74a43d1628ba444f8c76e09e6d346b
Pavel Platonov <devel_27@mail.ru>
2021-03-31 15:48:37 +0300

    Fix due to change of gitlab address

--- release.sh
+++ release.sh
@@ -31,15 +31,15 @@ DESCRIPTION="${DESCRIPTION}\n### Links\n";
 for i in ${!PROJECT_ID[@]}; do
 	if [[ "${PROJECT_ID[$i]}" != "68" ]]; then
 		if [[ "${PROJECT_ID[$i]}" != "4" ]]; then
-			JOB[$i]=$(curl --header "PRIVATE-TOKEN: ${CI_API_TOKEN}" "https://abris.site:8091/api/v4/projects/${PROJECT_ID[$i]}/jobs?scope=success" | jq '.[0]'); # JSON последнего успешного билда проекта.
+			JOB[$i]=$(curl --header "PRIVATE-TOKEN: ${CI_API_TOKEN}" "https://gitlab.abris.site/api/v4/projects/${PROJECT_ID[$i]}/jobs?scope=success" | jq '.[0]'); # JSON последнего успешного билда проекта.
 		else
-			JOB[$i]=$(curl --header "PRIVATE-TOKEN: ${CI_API_TOKEN}" "https://abris.site:8091/api/v4/projects/${PROJECT_ID[$i]}/jobs?scope=success" | jq 'map (select (.name=="build-free")) | .[0]'); # JSON последнего успешного билда сервера (больше одного job'а в конвеере).
+			JOB[$i]=$(curl --header "PRIVATE-TOKEN: ${CI_API_TOKEN}" "https://gitlab.abris.site/api/v4/projects/${PROJECT_ID[$i]}/jobs?scope=success" | jq 'map (select (.name=="build-free")) | .[0]'); # JSON последнего успешного билда сервера (больше одного job'а в конвеере).
 		fi
 		COMMIT[$i]=$(echo ${JOB[$i]} | jq --raw-output '.commit.short_id');
 		JOB[$i]=$(echo ${JOB[$i]} | jq '.id'); # Замена JSON'а на id процесса.
-		DESCRIPTION="${DESCRIPTION}* **${PROJECT_NAME[$i]}** [artifact](https://abris.site:8091/abris-lab/${PROJECT_NAME[$i]}/-/jobs/${JOB[$i]}/artifacts/download) (job [${JOB[$i]}](https://abris.site:8091/abris-lab/${PROJECT_NAME[$i]}/-/jobs/${JOB[$i]})), commit: https://abris.site:8091/abris-lab/${PROJECT_NAME[$i]}/commit/${COMMIT[$i]}\n"; # Добавление ссылки в описание.
+		DESCRIPTION="${DESCRIPTION}* **${PROJECT_NAME[$i]}** [artifact](https://gitlab.abris.site/abris-lab/${PROJECT_NAME[$i]}/-/jobs/${JOB[$i]}/artifacts/download) (job [${JOB[$i]}](https://gitlab.abris.site/abris-lab/${PROJECT_NAME[$i]}/-/jobs/${JOB[$i]})), commit: https://gitlab.abris.site/abris-lab/${PROJECT_NAME[$i]}/commit/${COMMIT[$i]}\n"; # Добавление ссылки в описание.
 	else
-		JOB[$i]=$(curl --header "PRIVATE-TOKEN: ${CI_API_TOKEN}" "https://abris.site:8091/api/v4/projects/${PROJECT_ID[$i]}/jobs?scope=success" | jq 'map (select (.name=="repack")) | .[0]'); # JSON последнего успешного деплоя до релиза.
+		JOB[$i]=$(curl --header "PRIVATE-TOKEN: ${CI_API_TOKEN}" "https://gitlab.abris.site/api/v4/projects/${PROJECT_ID[$i]}/jobs?scope=success" | jq 'map (select (.name=="repack")) | .[0]'); # JSON последнего успешного деплоя до релиза.
 		COMMIT[$i]=$(echo ${JOB[$i]} | jq --raw-output '.commit.short_id');
 		JOB[$i]=$(echo ${JOB[$i]} | jq '.id'); # Замена JSON'а на id процесса.
 	fi
@@ -51,5 +51,5 @@ echo
 # Release сreation
 echo "POST release data:"
 curl --header 'Content-Type: application/json' --header "Private-Token: ${CI_API_TOKEN}"\
-    --data "{ \"name\": \"${RELEASE_NAME}\", \"tag_name\": \"${R_VERSION}\", \"description\": \"${DESCRIPTION}\", \"assets\": { \"links\": [{ \"name\": \"abris-free\", \"url\": \"https://abris.site:8091/abris-lab/abris-free/-/jobs/${JOB[@]:(-1)}/artifacts/download\" }] } }"\
-    --request POST https://abris.site:8091/api/v4/projects/68/releases?ref=master | jq '.[]';
+    --data "{ \"name\": \"${RELEASE_NAME}\", \"tag_name\": \"${R_VERSION}\", \"description\": \"${DESCRIPTION}\", \"assets\": { \"links\": [{ \"name\": \"abris-free\", \"url\": \"https://gitlab.abris.site/abris-lab/abris-free/-/jobs/${JOB[@]:(-1)}/artifacts/download\" }] } }"\
+    --request POST https://gitlab.abris.site/api/v4/projects/68/releases?ref=master | jq '.[]';
--- repack.sh
+++ repack.sh
@@ -4,12 +4,12 @@ R_VERSION=$(grep 'Release version' release_description.md | grep -Po '\d{1,2}\.\
 
 cd /tmp/abris-free && rm -Rf ./*
 mkdir -p /tmp/abris-free/dist/images/ && cd /tmp/abris-free
-curl --output abris-ui.zip --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://abris.site:8091/api/v4/projects/5/jobs/artifacts/master/download?job=build-free";
+curl --output abris-ui.zip --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://gitlab.abris.site/api/v4/projects/5/jobs/artifacts/master/download?job=build-free";
 unzip ./abris-ui.zip
 cp -r ./packages/abris-ui/dist/* ./dist/
-curl --output abris-server-base.zip --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://abris.site:8091/api/v4/projects/69/jobs/artifacts/master/download?job=build";
+curl --output abris-server-base.zip --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://gitlab.abris.site/api/v4/projects/69/jobs/artifacts/master/download?job=build";
 unzip ./abris-server-base.zip -d ./dist/
-curl --output dist/Server/sql/pg_abris_free.sql --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://abris.site:8091/api/v4/projects/2/repository/files/pg_abris_free.sql/raw?ref=master";
+curl --output dist/Server/sql/pg_abris_free.sql --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://gitlab.abris.site/api/v4/projects/2/repository/files/pg_abris_free.sql/raw?ref=master";
 sed -i "s/VERSION/$R_VERSION/g" ./dist/version.json;
 sed -i "s/VERSION/$R_VERSION/g" ./dist/Server/version.json;
 sed -i "s/Test/Free/g" ./dist/version.json;

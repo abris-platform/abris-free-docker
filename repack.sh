8f0c598c5a4c7fe40fed0396832ce7a894bba73c
devel_27 <devel_27@mail.ru>
2021-05-17 10:58:34 +0300

    Change directory for taking pg_abris_free.sql

--- repack.sh
+++ repack.sh
@@ -4,12 +4,12 @@ R_VERSION=$(grep 'Release version' release_description.md | grep -Po '\d{1,2}\.\
 
 cd /tmp/abris-free && rm -Rf ./*
 mkdir -p /tmp/abris-free/dist/images/ && cd /tmp/abris-free
-curl --output abris-ui.zip --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://gitlab.abris.site/api/v4/projects/5/jobs/artifacts/master/download?job=build-free";
+curl --output abris-ui.zip --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://gitlab.abris.site/api/v4/projects/5/jobs/artifacts/master/download?job=build-free"; # Project ID: 5 == abris-ui
 unzip ./abris-ui.zip
 cp -r ./packages/abris-ui/dist/* ./dist/
-curl --output abris-server-base.zip --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://gitlab.abris.site/api/v4/projects/69/jobs/artifacts/master/download?job=build";
+curl --output abris-server-base.zip --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://gitlab.abris.site/api/v4/projects/69/jobs/artifacts/master/download?job=build"; # Project ID: 69 == abris-free-server
 unzip ./abris-server-base.zip -d ./dist/
-curl --output dist/Server/sql/pg_abris_free.sql --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://gitlab.abris.site/api/v4/projects/2/repository/files/pg_abris_free.sql/raw?ref=master";
+curl --output dist/Server/sql_install/pg_abris_free.sql --location --header "PRIVATE-TOKEN: xs9d1-T2G4pa73QsPKaf" "https://gitlab.abris.site/api/v4/projects/2/repository/files/pg_abris_free.sql/raw?ref=master"; # Project ID: 2 == pg_abris
 sed -i "s/VERSION/$R_VERSION/g" ./dist/version.json;
 sed -i "s/VERSION/$R_VERSION/g" ./dist/Server/version.json;
 sed -i "s/Test/Free/g" ./dist/version.json;
